c8cec018b849d20ca9326e13a99d8ebb2027dd67
younes ismaili <ismailiyounes20@gmail.com>
2025-06-15 18:07:31 +0100

    update++

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -7,6 +7,7 @@ case $index in
   0) vm_name="server-w0" ;;
   1) vm_name="server-w1" ;;
   2) vm_name="server-w2" ;;
+  3) vm_name="server-w3" ;;
   *) echo "Invalid index $index"; exit 1 ;;
 esac
 
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "vm_names" {
   type    = list(string)
-  default = ["server-w0", "server-w1", "server-w2"]
+  default = ["null", "null", "null", "null"]
 }
 
 variable "vm_ips" {
   type    = list(string)
-  default = ["192.168.56.110", "192.168.56.111", "192.168.56.112"]
+  default = ["0.0.0.0", "0.0.0.0", "0.0.0.0", "0.0.0.0"]
 }
